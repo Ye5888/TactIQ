bf48cf26454b3f7a1f0683dfc45ca4d5c2a55cc9
Songquing Ye <songye@Songquings-MacBook-Air-278.local>
2026-08-19 02:23:59 -0500

    Created App.tsx and transformed main file, corresponding adjustments

---
+++ frontend/src/main.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+const rootElement = document.getElementById('app');
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+createRoot(rootElement).render(<App />);
